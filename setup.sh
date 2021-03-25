7607c7831d7d55bf72039a54f204e3521ba3d5ab
iMashtak <deralus.ws@yandex.ru>
2021-03-25 21:52:59 +0300

    v0.1.4

--- setup.sh
+++ setup.sh
@@ -3,8 +3,9 @@
 LPTPATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 echo "> LPT directory detected: ${LPTPATH}"
+read -p "< Enter alias to automation tool of this local repository: " LPTLINK
 echo "> Setup lpt link in /usr/local/bin"
-ln -s "${LPTPATH}/run.sh" "/usr/local/bin/lpt"
+ln -s "${LPTPATH}/run.sh" "/usr/local/bin/${LPTLINK}"
 
 echo "> Pull required docker image"
 docker pull deralusws/latex-papers-template-image:1.0
